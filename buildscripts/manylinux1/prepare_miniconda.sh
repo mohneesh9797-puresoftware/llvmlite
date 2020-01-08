ed2025ddaea615e06121b95dc898d8bdaaa5984d
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-08 13:41:54 +0530

    Update prepare_miniconda.sh

--- buildscripts/manylinux1/prepare_miniconda.sh
+++ buildscripts/manylinux1/prepare_miniconda.sh
@@ -2,15 +2,9 @@
 set -e
 cd /root
 if [ `uname -m` = 'aarch64' ]; then
-   curl "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh"
-   chmod +x Archiconda3-0.2.3-Linux-aarch64.sh
-   bash Archiconda3-0.2.3-Linux-aarch64.sh -b -p $HOME/miniconda
-   export PATH="$HOME/miniconda/bin:$PATH"
-   sudo cp -r $HOME/miniconda/bin/* /usr/bin/
-   hash -r
-   sudo conda config --set always_yes yes --set changeps1 no
-   sudo conda update -q conda
-   sudo conda info -a
+   curl https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh > mini3.sh
+   chmod +x mini3.sh
+   bash mini3.sh -b -f
 else
   curl https://repo.continuum.io/miniconda/$1 > mini3.sh
   bash mini3.sh -b -f
