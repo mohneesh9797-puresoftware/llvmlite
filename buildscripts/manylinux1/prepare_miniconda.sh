0c97c72da0bf068aff065f1a0e5e52f5d83480a5
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-08 14:13:35 +0530

    Update prepare_miniconda.sh

--- buildscripts/manylinux1/prepare_miniconda.sh
+++ buildscripts/manylinux1/prepare_miniconda.sh
@@ -5,7 +5,7 @@ if [ `uname -m` = 'aarch64' ]; then
    yum install -y wget
    wget -q "https://github.com/Archiconda/build-tools/releases/download/0.2.3/Archiconda3-0.2.3-Linux-aarch64.sh" -O miniconda3.sh
    chmod +x miniconda3.sh
-   bash miniconda3.sh -b -f
+   bash miniconda3.sh -b -f -p /root/miniconda3
 else
   curl https://repo.continuum.io/miniconda/$1 > mini3.sh
   bash mini3.sh -b -f
