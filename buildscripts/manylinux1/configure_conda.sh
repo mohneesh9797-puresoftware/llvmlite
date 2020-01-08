4f83bbc99d30ef5cde058686c6283abf5df8166a
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-08 18:58:59 +0530

    Update configure_conda.sh

--- buildscripts/manylinux1/configure_conda.sh
+++ buildscripts/manylinux1/configure_conda.sh
@@ -4,7 +4,7 @@ if [ `uname -m` = 'aarch64' ]; then
   echo "conda ==4.5.12" >> /root/miniconda3/conda-meta/pinned
   echo "conda-build ==3.17.6" >> /root/miniconda3/conda-meta/pinned
 else
-  conda install -y anaconda-client
+  conda install -y conda =4.3.25 conda-build ==3.0.9 anaconda-client
   echo "conda ==4.3.25" >> /root/miniconda3/conda-meta/pinned
   echo "conda-build ==3.0.9" >> /root/miniconda3/conda-meta/pinned
 fi
