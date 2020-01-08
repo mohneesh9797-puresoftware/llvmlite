fe2172df4876f499349421ed98d80d21cda81555
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-01-08 12:33:33 +0530

    Update docker_run.sh

--- buildscripts/manylinux1/docker_run.sh
+++ buildscripts/manylinux1/docker_run.sh
@@ -4,5 +4,8 @@ srcdir=$( cd "$(dirname $0)/../.."  && pwd )
 echo "srcdir=$srcdir"
 
 echo "MINICONDA_FILE=$MINICONDA_FILE"
-docker run --rm -v $srcdir:/root/llvmlite quay.io/pypa/manylinux1_${ARCH} ${PRECMD} /root/llvmlite/buildscripts/manylinux1/$1 ${MINICONDA_FILE} $2
-
+if [ `uname -m` = 'aarch64' ]; then
+  docker run --rm -v $srcdir:/root/llvmlite qquay.io/pypa/manylinux2014_aarch64 ${PRECMD} /root/llvmlite/buildscripts/manylinux1/$1 ${MINICONDA_FILE} $2
+else
+  docker run --rm -v $srcdir:/root/llvmlite quay.io/pypa/manylinux1_${ARCH} ${PRECMD} /root/llvmlite/buildscripts/manylinux1/$1 ${MINICONDA_FILE} $2
+fi
